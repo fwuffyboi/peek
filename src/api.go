e238f1efb65654369f2a61bf6124d28eba53c19b
Эшли Карамель <biscuitisnotacookie@protonmail.com>
2023-12-05 23:04:06 +0000

    more progress to be done here.

--- src/api.go
+++ src/api.go
@@ -5,6 +5,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -16,9 +17,9 @@ func apiEndpoints(c *gin.Context) {
 		})
 	} else {
 		endpoints := map[string]string{
-			"/api":          "Show all API endpoints",
-			"/api/full":     "Show all API stats",
-			"/api/shutdown": "Shutdown the server",
+			"GET   /api":          "Show all API endpoints",
+			"GET   /api/full":     "Show all API stats",
+			"POST  /api/shutdown": "Shutdown the server",
 		}
 		// Send the JSON response
 		c.JSON(http.StatusOK, gin.H{"endpoints": endpoints})
@@ -88,6 +89,13 @@ func apiShutdownServer(c *gin.Context) {
 				c.JSON(http.StatusOK, gin.H{
 					"msg": c.ClientIP() + "has requested a server shutdown in " + shutdownDelay.String() + " seconds.",
 				})
+				time.Sleep(shutdownDelay)
+				cmd := exec.Command("shutdown", "-h", "now")
+
+				err := cmd.Run()
+				if err != nil {
+					log.Errorf("Error shutting down: %s", err)
+				}
 
 			} else {
 				c.JSON(http.StatusBadRequest, gin.H{
@@ -96,4 +104,41 @@ func apiShutdownServer(c *gin.Context) {
 			}
 		}
 	}
+}
+
+// Shutdown peek
+func shutdownPeek(c *gin.Context) {
+	if UnsupportedOS {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": "This operating system is not supported. Please use a Linux or Darwin(MacOS) derivative.",
+		})
+	} else {
+		if c.Request.Method != "POST" { // if not a post request
+			c.JSON(http.StatusMethodNotAllowed, gin.H{
+				"err": "To interact with this API endpoint, you must use a POST request.",
+			})
+		} else { // if is a post request
+			if c.Query("confirm") == "true" { // if ?confirm=true in url
+				c.JSON(http.StatusOK, gin.H{
+					"msg": c.ClientIP() + " has requested that Peek shuts down. Shutting down NOW!",
+				})
+
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"err": "You must confirm the application shutdown by adding ?confirm=true to the url.",
+				})
+			}
+
+		}
+	}
+}
+
+func shutdownMiddleware(c *gin.Context) {
+	defer func() {
+		log.Warnf("SHUTDOWN: %s has made a Peek shutdown request.", c.ClientIP())
+		log.Warn("<<Peek>> is shutting down...")
+		os.Exit(0)
+	}()
+
+	c.Next() // TODO: figure out how to make this work
 }
