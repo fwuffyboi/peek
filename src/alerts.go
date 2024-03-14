d71f80801dc1bcec8a345ac92e7a75dd5984f89a
Эшли Карамель <biscuitisnotacookie@protonmail.com>
2024-03-14 21:51:12 +0000

    feat: log when alert added

--- src/alerts.go
+++ src/alerts.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"time"
 )
 
 func addAlert(message string) {
-	alertsList[message] = time.Now()
+	var timeRN = time.Now()
+	alertsList[message] = timeRN
+	log.Infof("Added alert: message: %s, time: %q", message, timeRN)
 }
 
 func getAlerts() map[string]time.Time {
