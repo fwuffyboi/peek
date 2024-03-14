048411cb6c2fe1aeae088afe9d4e48cd7ee470d8
Эшли Карамель <biscuitisnotacookie@protonmail.com>
2024-03-14 19:34:02 +0000

    fix: ratelimiter serverIP and serverPort now display correct values

--- src/ratelimiter.go
+++ src/ratelimiter.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -24,10 +25,11 @@ type Info struct {
 }
 
 type DebugInfo struct {
-	ResetTime string `json:"resetTime"`
-	Limit     int    `json:"limit"`
-	ClientIP  string `json:"clientIP"`
-	ServerIP  string `json:"serverIP"`
+	ResetTime  string `json:"resetTime"`
+	Limit      int    `json:"limit"`
+	ClientIP   string `json:"clientIP"`
+	ServerIP   string `json:"serverIP"`
+	ServerPort string `json:"serverPort"`
 }
 
 func rlErrorHandler(c *gin.Context, info ratelimit.Info) {
@@ -41,10 +43,11 @@ func rlErrorHandler(c *gin.Context, info ratelimit.Info) {
 			strconv.Itoa(int(info.Limit)) + ") requests per second.",
 	}
 	debugJson := DebugInfo{
-		ResetTime: info.ResetTime.String(),
-		Limit:     int(info.Limit),
-		ClientIP:  c.ClientIP(),
-		ServerIP:  c.Request.Host,
+		ResetTime:  info.ResetTime.String(),
+		Limit:      int(info.Limit),
+		ClientIP:   c.ClientIP(),
+		ServerIP:   strings.Split(c.Request.Host, ":")[0], // remove port from server ip
+		ServerPort: strings.Split(c.Request.Host, ":")[1],
 	}
 
 	RLJson := RatelimitJson{
